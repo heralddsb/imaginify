622b009491002d13f54fb8f47039e1b8f1235211
heraldblrt <herald.balerite.work@gmail.com>
2024-05-31 19:06:57 -0400

    Added missing modules

--- src/components/shared/Collection.tsx
+++ src/components/shared/Collection.tsx
@@ -49,14 +49,14 @@ export const Collection = ({
   return (
     <>
       <div className="collection-heading">
-        <h2 className="h2-bold text-dark-600">Recent Edits</h2>
+        <h2 className="h2-bold text-dark-600">Latest Updates</h2>
         {hasSearch && <Search />}
       </div>
 
       {images.length > 0 ? (
         <ul className="collection-list">
           {images.map((image) => (
-            <Card image={image} key={image.title} />
+            <Card image={image} key={image._id} />
           ))}
         </ul>
       ) : (
